88c00ab84cc919e36bf457ebf6173c3854683b72
Carlos Garnacho <carlosg@gnome.org>
2014-04-21 19:11:19 +0200

    tools: Handle tablet axes in event-debug
    Signed-off-by: Carlos Garnacho <carlosg@gnome.org>
    Signed-off-by: Stephen Chandler Paul <thatslyude@gmail.com>

--- tools/event-debug.c
+++ tools/event-debug.c
@@ -312,6 +312,18 @@ print_axis_event(struct libinput_event *ev)
 	case LIBINPUT_POINTER_AXIS_HORIZONTAL_SCROLL:
 		ax = "hscroll";
 		break;
+	case LIBINPUT_POINTER_AXIS_DISTANCE:
+		ax = "distance";
+		break;
+	case LIBINPUT_POINTER_AXIS_PRESSURE:
+		ax = "pressure";
+		break;
+	case LIBINPUT_POINTER_AXIS_TILT_VERTICAL:
+		ax = "ytilt";
+		break;
+	case LIBINPUT_POINTER_AXIS_TILT_HORIZONTAL:
+		ax = "xtilt";
+		break;
 	default:
 		abort();
 	}
